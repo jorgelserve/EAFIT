d10b322f11b6489afedbdb3a1b23c17b25e8bbbb
Jorge Luis Serna <jorgelserve@icloud.com>
2018-02-10 10:51:58 -0500

    Commit taller 3

---
+++ academico/semestre_1/fundamentos_de_programacion/talleres/3/Taller3.java
@@ -0,0 +1,37 @@
+/*
+5.	Escriba un método llamado printTriangleType(int, int, int).
+El método recibe tres parámetros enteros que representan las longitudes de los lados de un
+triángulo e imprime el tipo de triángulo que se puede conformar con dichas longitudes. Por ejemplo:
+
+printTriangleType(5, 7, 7);
+printTriangleType(6, 6, 6);
+printTriangleType(5, 7, 8);
+printTriangleType(2, 18, 2);
+
+Las salidas son:
+	isósceles
+	equilátero
+	escaleno
+	isósceles
+
+El programa debe imprimir un error si no se puede formar un triángulo. Por ejemplo, si uno de los lados es mayor que la suma de los otros dos lados.
+*/
+
+public class Taller3 {
+	public static void main(String[] args) {
+        printTriangleType(5, 7, 7);
+    }
+    
+    public static void printTriangleType (int a, int b, int c) {
+        if(c != 0 || ) {
+            System.out.println("ejec");
+				 	
+        }
+    }
+}
+
+
+
+
+
+
