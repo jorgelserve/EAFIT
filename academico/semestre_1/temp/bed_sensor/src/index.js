6c5b4191167ccf737850a49320b59fbf276789b4
Jorge Luis Serna <jorgelserve@icloud.com>
2018-03-21 01:28:22 -0500

    avances gyro noche

--- academico/semestre_1/temp/bed_sensor/src/index.js
+++ academico/semestre_1/temp/bed_sensor/src/index.js
@@ -1,2 +1,22 @@
-require('smoothie')
-require('./data/socket.js')
+var socket = io.connect('http://localhost:8080', { 'forceNew': true })
+
+var smoothie = new SmoothieChart()
+smoothie.streamTo(document.getElementById("mycanvas"))
+
+// Data
+var x = new TimeSeries()
+var y = new TimeSeries()
+var z = new TimeSeries()
+
+// Add a random value to each line every second
+socket.on('gyro', function (data) {
+	// console.log(data)
+	x.append(new Date().getTime(), data.x)
+	y.append(new Date().getTime(), data.y)
+	z.append(new Date().getTime(), data.z)
+})
+
+// // Add to SmoothieChart
+smoothie.addTimeSeries(x)
+smoothie.addTimeSeries(y)
+smoothie.addTimeSeries(z)
