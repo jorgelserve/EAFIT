fb6f472f0febc533cc6d9c4c59e82ec420ce4e13
Jorge Luis Serna <jorgelserve@icloud.com>
2018-07-29 23:44:07 -0500

    Taller 1

--- academico/semestre_1/fundamentos_de_programacion/talleres/1/Taller1.java
+++ academico/semestre_1/fundamentos_de_programacion/talleres/1/Taller1.java
@@ -4,14 +4,52 @@
 */
 
 public class Taller1 {
-    public static void main (String[] args) {
-        imprimir(0);
-    }
-    
-    public static void imprimir (int control) {
-        if(control <= 244) {
-            System.out.println("Llamar metodos es un concepto poderoso");
-            imprimir(control + 1);
-        }
-    }
+
+	public static void ejercicio6 () {
+		decendente();
+		uno()
+		asendente();
+		System.out.println();
+		uno();
+		uno();
+		uno();
+		decendente();
+		uno();
+		dos();
+	}
+
+	public static void ejercicio7 () {
+		imprimir(0);
+	}
+
+	public static void imprimir (int control) {
+		if(control <= 244) {
+			System.out.println("Llamar metodos es un concepto poderoso");
+			imprimir(control + 1);
+		}
+	}
+
+	public static void cuatro () {
+		System.out.println("* * * *");
+	}
+
+	public static void dos () {
+		System.out.println("  * *");
+	}
+
+	public static void uno () {
+		System.out.println("    *");
+	}
+
+	public static void decendente () {
+		cuatro();
+		cuatro();
+		dos();
+	}
+
+	public static void asendente () {
+		dos();
+		cuatro();
+		cuatro();
+	}
 }
